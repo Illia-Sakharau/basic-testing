0129756d3a7998d4e5e8a918994ee8a7360a45ac
Illia Sakharau <sakharovia0204@gmail.com>
2024-02-11 19:36:50 +0400

    test: 08-snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,25 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const testResult = generateLinkedList(['val1', 'val2']);
+    expect(testResult).toStrictEqual({
+      next: {
+        next: {
+          next: null,
+          value: null,
+        },
+        value: 'val2',
+      },
+      value: 'val1',
+    });
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const testResult = generateLinkedList(['val1', 'val2', 'val3']);
+    expect(testResult).toMatchSnapshot();
   });
 });
